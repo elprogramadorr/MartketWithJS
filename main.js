ba8771df409aa769457a8b93698556fe69ea6cf0
mateoo <mateovaldivia3000@gmail.com>
2022-12-07 17:58:50 -0400

    added js to menus

--- main.js
+++ main.js
@@ -1,11 +1,31 @@
 const navEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const iconMenuApp = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const iconCart = document.querySelector('.navbar-shopping-cart');
+const carrito = document.querySelector('.product-detail');
 navEmail.addEventListener('click',showMenu);
+iconMenuApp.addEventListener('click',showMobileMenu);
+iconCart.addEventListener('click',showShoppingCart);
 function showMenu(event){
-    console.log("bebito fiu fiu ");
     if(desktopMenu.classList.contains('inactive')){
         desktopMenu.classList.remove('inactive');
     }else{
         desktopMenu.classList.add('inactive');
+    }
+}
+
+function showMobileMenu(){
+    if(mobileMenu.classList.contains('inactive')){
+        mobileMenu.classList.remove('inactive');
+        carrito.classList.add('inactive');
+    }else{
+        mobileMenu.classList.add('inactive');
+    }
+}
+function showShoppingCart(){
+    carrito.classList.toggle('inactive');
+    if(!carrito.classList.contains('inactive')){
+        mobileMenu.classList.add('inactive');
     }
 }
